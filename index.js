5940489daeb2af8c2661f2682efa0b6df5db34ac
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 02:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = {};
+
+  for (var i in object) {
+    obj[i] = object[i];
+  }
+  obj[key] = value;
+
+  return obj;
+}
